0eb6cb3cd40cd5ec254276419293f10f9f8fa5c5
Steven Dao <iamstevendao@gmail.com>
2017-10-12 14:44:36 +1100

    add view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,16 +6,20 @@ namespace HelloWorldAspNetCore.Controllers
   public class HelloWorldController : Controller
   {
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-      return "This is my default action...";
+      return View();
     }
 
     // GET: /HelloWorld/Welcome/
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
       //return "This is the Welcome method...";
-      return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is {ID}");
+      //return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is {ID}");
+      ViewData["Message"] = "Hello " + name;
+      ViewData["NumTimes"] = numTimes;
+
+      return View();
     }
   }
 }
