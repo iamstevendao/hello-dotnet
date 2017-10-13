3eef1c6fbc20a0556796785e016546291bd43d88
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-14 03:52:25 +1100

    add data validation in movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,12 +6,27 @@ namespace HelloWorldAspNetCore.Models
   public class Movie
   {
     public int ID { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string Title { get; set; }
+
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [DataType(DataType.Date)]
     public DateTime RealeaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+    [StringLength(30)]
+    [Required]
     public string Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+    [StringLength(5)]
+    [Required]
     public string Rating { get; set; }
   }
 }
