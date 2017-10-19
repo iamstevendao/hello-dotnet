7c4ca679e9f3dab616c3c2dcc5829e0613442013
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-19 07:42:04 +0700

    adding details and edit

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -10,5 +10,39 @@ namespace HelloWorldAspNetCore.Controllers
     {
       return View();
     }
+
+    // GET: Person/Details/5
+    // public async Task<IActionResult> Details(int? id)
+    // {
+    //   if (id == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   var person = await _context.Movie
+    //       .SingleOrDefaultAsync(m => m.ID == id);
+    //   if (person == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   return View(person);
+    // }
+
+    // GET: Person/Edit/5
+    // public async Task<IActionResult> Edit(int? id)
+    // {
+    //   if (id == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
+    //   if (movie == null)
+    //   {
+    //     return NotFound();
+    //   }
+    //   return View(movie);
+    // }
   }
 }
--- wwwroot/js/personCtrl.js
+++ wwwroot/js/personCtrl.js
@@ -7,5 +7,12 @@
         { firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com' },
         { firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com' }
       ];
+
+      $scope.removeItem = function removeItem (row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+          $scope.rowCollection.splice(index, 1);
+        }
+      }
     }])
 })(window.angular)
