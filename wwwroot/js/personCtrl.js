da6e16b5d5ec1b2586b42e52c9210fa5170d0d55
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-23 23:07:01 +0700

    done edit

--- wwwroot/js/personCtrl.js
+++ wwwroot/js/personCtrl.js
@@ -3,33 +3,46 @@
   angular.module('PersonManagementApp', ['smart-table'])
     .controller('PersonManagementCtrl', ['$scope', function ($scope) {
       $scope.rowCollection = [
-        { name: 'Laurent Renard', dob: '1987-05-21', email: 'whatever@gmail.com' },
-        { name: 'Blandine Faivre', dob: '1987-04-25', email: 'oufblandou@gmail.com' },
-        { name: 'Francoise Frere', dob: '1955-08-27', email: 'raymondef@gmail.com' }
+        { name: 'Laurent Renard', dob: new Date(1987, 5, 21), email: 'whatever@gmail.com' },
+        { name: 'Blandine Faivre', dob: new Date(1987, 5, 21), email: 'oufblandou@gmail.com' },
+        { name: 'Francoise Frere', dob: new Date(1987, 5, 21), email: 'raymondef@gmail.com' }
       ]
 
       $scope.currentItem = {}
+      let index = -1
 
-      $scope.deleteItem = function deleteItem (row) {
-        var index = $scope.rowCollection.indexOf(row)
+      $scope.reset = function () {
+        $scope.clone = angular.copy($scope.currentItem)
+      }
+
+      $scope.deleteItem = function (row) {
+        getCurrentItem(row)
         if (index !== -1) {
           $scope.rowCollection.splice(index, 1)
         }
       }
 
-      $scope.editItem = function editItem (row) {
-        var index = $scope.rowCollection.indexOf(row)
-        $scope.currentItem = row
+      $scope.editItem = function (row) {
+        getCurrentItem(row)
+        $scope.reset()
+        $('#myModal').modal('show')
+      }
+
+      $scope.detailItem = function (row) {
+        getCurrentItem(row)
         if (index !== -1) {
           $('#myModal').modal('show')
         }
       }
 
-      $scope.detailItem = function detailItem (row) {
-        var index = $scope.rowCollection.indexOf(row)
-        if (index !== -1) {
-          $('#myModal').modal('show')
-        }
+      function getCurrentItem (row) {
+        index = $scope.rowCollection.indexOf(row)
+        $scope.currentItem = row
+      }
+
+      $scope.update = function () {
+        $scope.rowCollection[index] = $scope.clone
+        $('#myModal').modal('hide')
       }
     }])
 })(window.angular)
