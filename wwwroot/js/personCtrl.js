d7498aacabfde7b7d96be354e083a25e043c1897
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-23 18:51:57 +0700

    working on edit form

--- wwwroot/js/personCtrl.js
+++ wwwroot/js/personCtrl.js
@@ -1,22 +1,25 @@
 (function (angular) {
   'use strict'
   angular.module('PersonManagementApp', ['smart-table'])
-    .controller("PersonManagementCtrl", ['$scope', function ($scope) {
+    .controller('PersonManagementCtrl', ['$scope', function ($scope) {
       $scope.rowCollection = [
-        { firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com' },
-        { firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com' },
-        { firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com' }
-      ];
+        { name: 'Laurent Renard', dob: '1987-05-21', email: 'whatever@gmail.com' },
+        { name: 'Blandine Faivre', dob: '1987-04-25', email: 'oufblandou@gmail.com' },
+        { name: 'Francoise Frere', dob: '1955-08-27', email: 'raymondef@gmail.com' }
+      ]
+
+      $scope.currentItem = {}
 
       $scope.deleteItem = function deleteItem (row) {
-        var index = $scope.rowCollection.indexOf(row);
+        var index = $scope.rowCollection.indexOf(row)
         if (index !== -1) {
-          $scope.rowCollection.splice(index, 1);
+          $scope.rowCollection.splice(index, 1)
         }
       }
 
       $scope.editItem = function editItem (row) {
         var index = $scope.rowCollection.indexOf(row)
+        $scope.currentItem = row
         if (index !== -1) {
           $('#myModal').modal('show')
         }
